42f68e2cf8ccf20eb6cdba9a1b7f19257e656067
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2023-07-25 22:56:18 +0200

    Added messaging and client role assignments

--- Terraform/role_assignments.tf
+++ Terraform/role_assignments.tf
@@ -4,8 +4,12 @@ module "role-assignments" {
 
   role_assignments = [
     {
-      scope      = module.container-registry.id
-      sp_names   = ["sampleapp-core-githubactions"]
+      scope = module.container-registry.id
+      sp_names = [
+        "sampleapp-core-githubactions",
+        "sampleapp-messaging-githubactions",
+        "sampleapp-client-githubactions"
+      ]
       role_names = ["AcrPush"]
     }
   ]
