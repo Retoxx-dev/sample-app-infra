4d5f4ba1610a8245405a934992f94e8713db1f16
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2023-08-10 14:32:53 +0200

    temporarly disabled key vault secrets module

--- Terraform/key_vault_secrets.tf
+++ Terraform/key_vault_secrets.tf
@@ -1,11 +1,11 @@
-module "key-vault-secrets" {
-  source  = "Retoxx-dev/keyvault-secrets/azurerm"
-  version = "1.0.0"
+# module "key-vault-secrets" {
+#   source  = "Retoxx-dev/keyvault-secrets/azurerm"
+#   version = "1.0.0"
 
-  key_vault_id = module.key-vault.id
-  secrets = [
-    {
+#   key_vault_id = module.key-vault.id
+#   secrets = [
+#     {
 
-    }
-  ]
-}
+#     }
+#   ]
+# }
