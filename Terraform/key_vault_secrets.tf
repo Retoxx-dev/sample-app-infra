67dab3b1f069c7ca9a728faa44c752155c55365c
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2023-08-10 14:31:37 +0200

    Fix to tf format

--- Terraform/key_vault_secrets.tf
+++ Terraform/key_vault_secrets.tf
@@ -2,10 +2,10 @@ module "key-vault-secrets" {
   source  = "Retoxx-dev/keyvault-secrets/azurerm"
   version = "1.0.0"
 
-    key_vault_id = module.key-vault.id
-    secrets = [
-        {
-            
-        }
-    ]
+  key_vault_id = module.key-vault.id
+  secrets = [
+    {
+
+    }
+  ]
 }
