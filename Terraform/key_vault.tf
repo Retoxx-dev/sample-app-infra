7c53d99eec75eff42b797777aabdf566af21e3a4
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2023-08-10 14:26:30 +0200

    Modified aks, virtual network and added keyvault with secrets

---
+++ Terraform/key_vault.tf
@@ -0,0 +1,43 @@
+module "key-vault" {
+  source  = "Retoxx-dev/keyvault/azurerm"
+  version = "1.0.0"
+
+  name                = "kv-${local.naming_convention_dash}"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  sku_name = "standard"
+
+  public_network_access_enabled = true
+
+  access_policies = [
+    {
+      azure_ad_user_upns      = ["kacper@retoxxdev.onmicrosoft.com"]
+      key_permissions         = ["Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"]
+      secret_permissions      = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
+      certificate_permissions = ["Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"]
+      storage_permissions     = ["Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"]
+    },
+    {
+      azure_ad_service_principal_names = ["aks-sample-app-neu-agentpool"]
+      secret_permissions               = ["Get", "List"]
+    }
+  ]
+
+  self_service_principal_id = "6b0481ff-2883-4ac9-ba47-3a8d7ba65d5f"
+
+  network_acls = {
+    bypass                     = "AzureServices"
+    default_action             = "Deny"
+    ip_rules                   = ["185.157.14.184"]
+    virtual_network_subnet_ids = []
+  }
+
+  private_endpoint = {
+    name                          = "pep-kv-${local.naming_convention_dash}"
+    subnet_id                     = module.virtual-network.subnet_ids["snet-${local.naming_convention_dash}-aks"]
+    custom_network_interface_name = "nic-kv-${local.naming_convention_dash}"
+
+    private_dns_zone_id = azurerm_private_dns_zone.kv.id
+  }
+}
--- Terraform/virtual_network.tf
+++ Terraform/virtual_network.tf
@@ -44,3 +44,16 @@ resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   resource_group_name   = azurerm_resource_group.this.name
   virtual_network_id    = module.virtual-network.id
 }
+
+#Temp untill private dns zone module
+resource "azurerm_private_dns_zone" "kv" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "kv" {
+  name                  = "vdn-kv-${local.naming_convention_dash}"
+  private_dns_zone_name = azurerm_private_dns_zone.kv.name
+  resource_group_name   = azurerm_resource_group.this.name
+  virtual_network_id    = module.virtual-network.id
+}
