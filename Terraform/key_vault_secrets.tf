51df54f5eff78088ae2ae0a6fabae4856842a1ef
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2023-08-10 14:37:11 +0200

    [DO-7] Modification of argocd and terraform infrastructure (#8)
    * Modified workflow trigger paths
    
    * Added .env files to gitignore
    
    * Modified aks, virtual network and added keyvault with secrets
    
    * Modified argo taskfile, deleted unused manifests
    
    * Fix to tf format
    
    * temporarly disabled key vault secrets module

---
+++ Terraform/key_vault_secrets.tf
@@ -0,0 +1,11 @@
+# module "key-vault-secrets" {
+#   source  = "Retoxx-dev/keyvault-secrets/azurerm"
+#   version = "1.0.0"
+
+#   key_vault_id = module.key-vault.id
+#   secrets = [
+#     {
+
+#     }
+#   ]
+# }
--- Terraform/kubernetes_cluster.tf
+++ Terraform/kubernetes_cluster.tf
@@ -1,47 +1,47 @@
-# module "kubernetes-cluster" {
-#   source  = "Retoxx-dev/kubernetes-cluster/azurerm"
-#   version = "1.0.0"
+module "kubernetes-cluster" {
+  source  = "Retoxx-dev/kubernetes-cluster/azurerm"
+  version = "1.0.0"
 
-#   resource_group_name = azurerm_resource_group.this.name
-#   location            = local.location
+  resource_group_name = azurerm_resource_group.this.name
+  location            = local.location
 
-#   name                = "aks-${local.naming_convention_dash}"
-#   dns_prefix          = "aks-${local.naming_convention_dash}"
-#   node_resource_group = "rg-${local.naming_convention_dash}-aks-nodes"
+  name                = "aks-${local.naming_convention_dash}"
+  dns_prefix          = "aks-${local.naming_convention_dash}"
+  node_resource_group = "rg-${local.naming_convention_dash}-aks-nodes"
 
-#   kubernetes_version = "1.25.6"
-#   sku_tier           = "Free"
+  kubernetes_version = "1.25.6"
+  sku_tier           = "Free"
 
-#   role_based_access_control_enabled = true
+  role_based_access_control_enabled = true
 
-#   api_server_access_profile = {
-#     authorized_ip_ranges = ["185.157.14.184/32"]
-#   }
-#   public_network_access_enabled  = true
+  api_server_access_profile = {
+    authorized_ip_ranges = ["185.157.14.184/32"]
+  }
+  public_network_access_enabled = true
 
-#   network_profile = {
-#     network_plugin = "azure"
-#     dns_service_ip = "10.2.0.10"
-#     outbound_type  = "loadBalancer"
-#     service_cidr   = "10.2.0.0/24"
-#   }
+  network_profile = {
+    network_plugin = "azure"
+    dns_service_ip = "10.2.0.10"
+    outbound_type  = "loadBalancer"
+    service_cidr   = "10.2.0.0/24"
+  }
 
-#   identity = {
-#     name = "aks-${local.naming_convention_dash}"
-#   }
+  identity = {
+    name = "aks-${local.naming_convention_dash}"
+  }
 
-#   default_node_pool = {
-#     name                   = "default"
-#     vm_size                = "standard_B2s"
-#     vnet_subnet_id         = module.virtual-network.subnet_ids["snet-${local.naming_convention_dash}-aks"]
-#     min_count              = 2
-#     max_count              = 3
-#     enable_auto_scaling    = true
-#     enable_node_public_ip  = false
-#     max_pods               = 110
-#     orchestrator_version   = "1.25.6"
-#   }
-# }
+  default_node_pool = {
+    name                  = "default"
+    vm_size               = "standard_D2as_v4"
+    vnet_subnet_id        = module.virtual-network.subnet_ids["snet-${local.naming_convention_dash}-aks"]
+    min_count             = 2
+    max_count             = 3
+    enable_auto_scaling   = true
+    enable_node_public_ip = false
+    max_pods              = 110
+    orchestrator_version  = "1.25.6"
+  }
+}
 
 # module "kubernetes-cluster-node-pools" {
 #   source  = "Retoxx-dev/kubernetes-cluster-node-pools/azurerm"
