fb81814a034e71c61a5fb469d6adc4ee0e5bb68a
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2023-08-19 15:15:25 +0200

    Added dns zone config

---
+++ Terraform/dns_zone.tf
@@ -0,0 +1,21 @@
+module "dns-zone" {
+  source = "Retoxx-dev/dns-zone/azurerm"
+  version = "1.0.1"
+
+  resource_group_name = azurerm_resource_group.this.name
+
+  dns_zone_name = "dziedzic.me"
+
+  a_records = [
+    {
+      name = "@"
+      ttl = 300
+      target_resource_id = one(module.kubernetes-cluster.effective_outbound_ips)
+    },
+    {
+      name = "*"
+      ttl = 300
+      target_resource_id = one(module.kubernetes-cluster.effective_outbound_ips)
+    }
+  ]
+}
